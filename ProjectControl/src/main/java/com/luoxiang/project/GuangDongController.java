3e9e264ac40c706edbb14beb0006eb12aefc2e97
vincent <ynztlxdeai@163.com>
2021-01-30 12:07:22 +0800

    <feat> update guang dong 2021 01

---
+++ ProjectControl/src/main/java/com/luoxiang/project/GuangDongController.java
@@ -0,0 +1,24 @@
+package com.luoxiang.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * projectName: 	    com.luoxiang.parent
+ * packageName:	        com.luoxiang.project
+ * className:	        GuangDongController
+ * author:	            Luoxiang
+ * time:	            2021/1/30	11:40
+ * desc:
+ *
+ * svnVersion:	        $Rev
+ * upDateAuthor:	    Vincent
+ * upDate:	            2021/1/30
+ * upDateDesc:
+ */
+
+@Controller
+@RequestMapping("guangdong")
+public class GuangDongController {
+
+}
---
+++ ProjectService/src/main/java/com/luoxiang/project/service/GuangDongService.java
@@ -0,0 +1,30 @@
+package com.luoxiang.project.service;
+
+import com.luoxiang.project.bean.CommBean;
+import com.luoxiang.project.po.GuangDong01;
+
+import org.apache.poi.ss.formula.functions.T;
+
+import java.util.List;
+
+/**
+ * projectName: 	    com.luoxiang.parent
+ * packageName:	        com.luoxiang.project.service
+ * className:	        GuangDongService
+ * author:	            Luoxiang
+ * time:	            2021/1/30	11:49
+ * desc:	            TODO
+ *
+ * svnVersion:	        $Rev
+ * upDateAuthor:	    Vincent
+ * upDate:	            2021/1/30
+ * upDateDesc:	        TODO
+ */
+
+
+public interface GuangDongService {
+
+    List<GuangDong01> selectAll();
+
+    CommBean<T> update();
+}

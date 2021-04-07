380cd49d7be2684b3fdbbedddf34af69aec016ba
vincent <ynztlxdeai@163.com>
2021-04-07 15:10:37 +0800

    <feat> update jd 2021

--- ProjectService/src/main/java/com/luoxiang/project/service/impl/ArmyServiceImpl.java
+++ ProjectService/src/main/java/com/luoxiang/project/service/impl/ArmyServiceImpl.java
@@ -5,12 +5,27 @@ import com.luoxiang.project.mapper.Jd2021Mapper;
 import com.luoxiang.project.po.Jd2021;
 import com.luoxiang.project.service.ArmyService;
 
+import net.sf.json.JSONObject;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.util.TextUtils;
 import org.apache.poi.ss.formula.functions.T;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -34,9 +49,67 @@ public class ArmyServiceImpl implements ArmyService{
     @Resource
     public Jd2021Mapper jd2021Mapper;
 
+    public HashMap<String , Jd2021> initMap(){
+        HashMap<String , Jd2021> map = new HashMap<>();
+        List<Jd2021> list = selectAll2021();
+        for (Jd2021 t : list){
+            map.put(t.getJobCode() , t);
+        }
+        return map;
+    }
+
     @Override
     public CommBean<T> update2021() {
-        return null;
+        CommBean c = new CommBean();
+        HashMap<String , Jd2021> map = initMap();
+
+        String examid = "5083fd4b2a75e4d1";
+        String cookies = "JSESSIONID=FE82BA4F1049BEBC96F832EE83BCF6C3; JSESSIONID=087241904D3AE5A47AF288F0B2A6DB3A; Hm_lvt_ad65617c1b92d555455e06647127f54e=1617764738,1617764947,1617777720; Hm_lpvt_ad65617c1b92d555455e06647127f54e=1617777720; SERVERID=a3f706d4e03122459119f5fb5c8f0be8|1617777746|1617777717";
+        String url = "http://182.92.48.172/qjwzwb/stuchooseexam/getPositionInfo.htm";
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost   httpPost   = new HttpPost(url);
+        httpPost.addHeader("Accept" , "*/*");
+        httpPost.addHeader("Accept-Encoding" , "gzip, deflate");
+        httpPost.addHeader("Accept-Language" , "zh-CN,zh;q=0.9");
+        httpPost.addHeader("Connection" , "keep-alive");
+        httpPost.addHeader("Content-Type" , "application/x-www-form-urlencoded; charset=UTF-8");
+        httpPost.addHeader("Cookie" ,cookies );
+        httpPost.addHeader("Host" , "182.92.48.172");
+        httpPost.addHeader("Origin" , "http://182.92.48.172");
+        httpPost.addHeader("Referer" , "http://182.92.48.172/qjwzwb/stuchooseexam/selectPosition.htm?examstupid=1015&userid=eba0061d8b638835&bmid=3f74e811b87eddc5&examid=5083fd4b2a75e4d1");
+        httpPost.addHeader("User-Agent" , "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36");
+        httpPost.addHeader("X-Requested-With" , "XMLHttpRequest");
+
+        Iterator<Map.Entry<String, Jd2021>> iterator = map.entrySet().iterator();
+        Jd2021 tmp = null;
+        while (iterator.hasNext()){
+            tmp = iterator.next().getValue();
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            nvps.add(new BasicNameValuePair("zwdm", tmp.getJobCode()));
+            nvps.add(new BasicNameValuePair("examid", examid));
+            try {
+                httpPost.setEntity(new UrlEncodedFormEntity(nvps, "utf-8"));
+                HttpResponse response = httpClient.execute(httpPost);
+                HttpEntity   entity   = response.getEntity();
+                if (entity != null){
+                    String       s        = EntityUtils.toString(entity, "UTF-8");
+                    if (TextUtils.isEmpty(s)){
+                        continue;
+                    }
+                    JSONObject j = JSONObject.fromObject(s);
+                    int num = j.getInt("zwbkrs");
+                    tmp.setHasNum(num);
+                    StringBuffer buffer = new StringBuffer(tmp.getIngNum());
+                    buffer.append(TextUtils.isEmpty(tmp.getIngNum()) ? "" : "," ).append(num + "");
+                    tmp.setIngNum(buffer.toString());
+                    jd2021Mapper.updateByPrimaryKey(tmp);
+                }
+            }catch (Exception e){
+
+            }
+        }
+
+        return c;
     }
 
     @Override

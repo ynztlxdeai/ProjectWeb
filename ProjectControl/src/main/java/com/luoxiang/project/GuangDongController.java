b2a9a7d76a4de892db39b68ad15d025402c55ec0
vincent <ynztlxdeai@163.com>
2022-05-24 22:05:55 +0800

    <feat> finish gd

--- ProjectControl/src/main/java/com/luoxiang/project/GuangDongController.java
+++ ProjectControl/src/main/java/com/luoxiang/project/GuangDongController.java
@@ -1,5 +1,6 @@
 package com.luoxiang.project;
 
+import com.luoxiang.project.po.GdSydw2022;
 import com.luoxiang.project.po.GuangDong01;
 import com.luoxiang.project.service.GuangDongService;
 
@@ -85,17 +86,17 @@ public class GuangDongController {
 
     @RequestMapping("update/sydw")
     public String updateSydw(Model model ,String cookies ,  int cmp , boolean filter , boolean skip , String time){
-        //http://localhost:8080/guangdong/update?skip=false&time=1638165322590&filter=false&cmp=5&cookies=JSESSIONID=B81qQwjtb2hOKmQloSfYx_tpZPfVDLlfBJpHUWmWnSeZ6MVH98GO!1327406713
+        //http://localhost:8080/guangdong/update/sydw?skip=false&time=1648510584380&filter=false&cmp=5&cookies=JSESSIONID=QJHS1XMmLAoRHbcqFnY55v9WUX044atvv-OgLuoF7rMfp8Zbdzof!1907289670; JSESSIONID=zuDQ6nNm_OnM3_5c2xGVE9OapqDns5yz28f3i7EulmWPMZM-yN4t!-331356933
         if (!skip){
-            guangDongServiceImpl.update(cookies , time);
+            guangDongServiceImpl.updateSydw(cookies , time);
         }
 
-        List<GuangDong01> all = guangDongServiceImpl.sortAll(cmp , filter);
+        List<GdSydw2022> all = guangDongServiceImpl.sortAllSydw(cmp , filter);
 
 
         StringBuffer            stringBuffer = new StringBuffer();
         stringBuffer.append("<ul>");
-        for (GuangDong01 gov : all) {
+        for (GdSydw2022 gov : all) {
             stringBuffer.append("<li>");
             stringBuffer.append(gov.showData());
             stringBuffer.append("</li>");
